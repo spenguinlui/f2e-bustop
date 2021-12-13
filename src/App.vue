bcc969b70848e247e3642a35a30b51759ddbb3a9
spenguin10lui <spenguin10@yahoo.com.tw>
2021-12-13 14:28:41 +0800

    增加手機禁止橫向

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,7 @@
       </div>
     </footer>
     <Map />
+    <MobileOrientation v-if="isHorizontal"/>
   </div>
 </template>
 
@@ -21,16 +22,42 @@ import LandingPage from "./pages/landing-page-m.vue";
 import ListBoard from "./components/list-board.vue";
 import ListBoardMobile from "./components/list-board-m.vue";
 import KeyboardMobile from "./components/key-board-m.vue";
+import MobileOrientation from "./pages/mobile-orientation.vue";
 
 export default {
   name: "App",
+  data() {
+    return {
+      isHorizontal: false
+    }
+  },
   components: {
     Map,
     NavBar,
     LandingPage,
     ListBoard,
     ListBoardMobile,
-    KeyboardMobile
+    KeyboardMobile,
+    MobileOrientation
+  },
+  methods: {
+    orientationchange() {
+      const width = document.documentElement.clientWidth;
+      const height = document.documentElement.clientHeight;
+      if (width < height) {
+        this.isHorizontal = true;
+        console.log(`width: ${width}, height: ${height}`)
+      } else {
+        this.isHorizontal = false;
+        console.log(`width: ${width}, height: ${height}`)
+      }
+    }
+  },
+  mounted() {
+    window.addEventListener("orientationchange", this.orientationchange, false);
+  },
+  beforeDestroyed(){
+    window.removeEventListener("orientationchange", this.orientationchange, false);
   }
 }
 </script>
---
+++ src/pages/mobile-orientation.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="keepmsg">
+    <div class="msg">為了確保最佳的使用體驗，請將您的螢幕轉為直向，謝謝！</div>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss" scoped>
+  @import "@/assets/scss/main.scss";
+
+  .keepmsg {
+    @include posi(f);
+    @include flex-row-center-center;
+    width: 100vw;
+    height: 100vh;
+    background: $primary-300;
+    color: $grey-100;
+    z-index: 1000;
+    .msg {
+      @include font-h5(bold);
+    }
+  }
+</style>
